c8e4667d2bf56ef14508cb34b3429f8b9c3b6cab
nanjohtk <36926075+nanjohtk@users.noreply.github.com>
2018-03-01 11:27:38 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'Chika') {
+    	message.reply('cyka blyat');
   	}
 });
 
