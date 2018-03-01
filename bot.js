14c6f47c2e52212a82734b9dfba41c7125d8d60e
nanjohtk <36926075+nanjohtk@users.noreply.github.com>
2018-03-01 11:19:13 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready' , () => {
-  console.log('I am ready!');
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-  if (message.content === 'Chika') {
-    message.reply('cyka blyat');
-    }
+    if (message.content === 'Chika') {
+    	message.reply('cyka blyat');
+  	}
 });
 
-// THIS MUST BE THIS WAY
-Client.login(process.env.BOT_TOKEN);
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
